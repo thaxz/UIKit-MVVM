b8d447c31472d858aba9c526cbceb3e97e9ee6c9
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-29 17:54:03 -0300

    first commit

---
+++ MVVMUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMUIKit
+//
+//  Created by thaxz on 29/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

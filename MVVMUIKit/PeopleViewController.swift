ac19e80c92e42baa7748fa53dda35a08c5d3b1d4
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-30 17:56:20 -0300

    feature(Develop): Starting to create components

--- MVVMUIKit/PeopleViewController.swift
+++ MVVMUIKit/PeopleViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class PeopleViewController: UIViewController {
+    
+    //MARK: Components
+    
+    private lazy var subscribeBtn: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "SUBSCRIBE"
+        config.baseBackgroundColor = .red
+        config.baseForegroundColor = .white
+        config.buttonSize = .large
+        config.cornerStyle = .medium
+        let btn = UIButton(configuration: config)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +35,7 @@ private extension PeopleViewController {
     
     func setup(){
         self.view.backgroundColor = .white
+        self.view.addSubview(subscribeBtn)
     }
     
 }

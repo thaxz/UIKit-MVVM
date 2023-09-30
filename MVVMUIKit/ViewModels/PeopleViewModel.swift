661d6ad1a57b42b24943b3f9518c0e8ec7e00b06
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-30 19:07:33 -0300

    feature(Develop): starting viewmodel

---
+++ MVVMUIKit/ViewModels/PeopleViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  PeopleViewModel.swift
+//  MVVMUIKit
+//
+//  Created by thaxz on 30/09/23.
+//
+
+import Foundation
+
+class PeopleViewModel {
+    
+    private var people: [PersonResponse] = []
+    
+    func getUsers(){
+        Task { [weak self] in
+            let url = URL(string:"https://reqres.in/api/users")!
+            do {
+                let (data, _) =  try await URLSession.shared.data(from: url)
+                // decoding from snake case
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                // getting data
+                self?.people = try  jsonDecoder.decode(UsersResponse.self, from: data).data
+                
+            } catch {
+                print(error)
+            }
+            
+        }
+    }
+    
+    
+    
+}

f654b2f5c9f2d848b5778095ed53410a3354c25d
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-30 18:44:58 -0300

    feature(Develop): extracting view to a separate file to start componetization process

--- MVVMUIKit/PeopleViewController.swift
+++ MVVMUIKit/PeopleViewController.swift
@@ -10,51 +10,11 @@ import UIKit
 class PeopleViewController: UIViewController {
     
     //MARK: Components
-    
-    private lazy var subscribeBtn: UIButton = {
-        var config = UIButton.Configuration.filled()
-        config.title = "SUBSCRIBE"
-        config.baseBackgroundColor = .red
-        config.baseForegroundColor = .white
-        config.buttonSize = .large
-        config.cornerStyle = .medium
-        let btn = UIButton(configuration: config)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.addTarget(self, action: #selector(didTapSubscribe), for: .touchUpInside)
-        return btn
-    }()
 
-    private lazy var personContainerView: UIView = {
-       let vw = UIView()
-        vw.layer.cornerRadius = 10
-        vw.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
-        vw.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var personView: PersonView = {
+       let vw = PersonView()
         return vw
     }()
-    
-    private lazy var nameLb: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Taylor Swift"
-        label.font = .systemFont(ofSize: 24, weight: .bold)
-        return label
-    }()
-    
-    private lazy var emailLb: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "tswift13@gmail.com"
-        label.font = .systemFont(ofSize: 15, weight: .regular)
-        return label
-    }()
-    
-    private lazy var personStackView: UIStackView = {
-           let vw = UIStackView()
-            vw.axis = .vertical
-            vw.spacing = 8
-            vw.translatesAutoresizingMaskIntoConstraints = false
-            return vw
-        }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,35 +29,16 @@ private extension PeopleViewController {
     
     func setup(){
         self.view.backgroundColor = .white
-        
-        self.view.addSubview(personContainerView)
-        
-        personContainerView.addSubview(personStackView)
-        // to work as a VStack
-        personStackView.addArrangedSubview(nameLb)
-        personStackView.addArrangedSubview(emailLb)
-        personStackView.addArrangedSubview(subscribeBtn)
+        self.view.addSubview(personView)
         setConstraints()
     }
     
     func setConstraints(){
         NSLayoutConstraint.activate([
             // using container to have a color background
-            personContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            personContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            personContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            
-            // STACK
-            personStackView.topAnchor.constraint(equalTo: personContainerView.topAnchor, constant: 8),
-            personStackView.bottomAnchor.constraint(equalTo: personContainerView.bottomAnchor, constant: -8),
-            personStackView.leadingAnchor.constraint(equalTo: personContainerView.leadingAnchor, constant: 8),
-            personStackView.trailingAnchor.constraint(equalTo: personContainerView.trailingAnchor, constant: -8),
-            
+            personView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            personView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            personView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
         ])
     }
-    
-    @objc func didTapSubscribe(sender: UIButton){
-        print("subscribing")
-    }
-    
 }
---
+++ MVVMUIKit/PersonView.swift
@@ -0,0 +1,92 @@
+//
+//  PersonView.swift
+//  MVVMUIKit
+//
+//  Created by thaxz on 30/09/23.
+//
+
+import UIKit
+
+class PersonView: UIView {
+
+    //MARK: Components
+    
+    private lazy var subscribeBtn: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "SUBSCRIBE"
+        config.baseBackgroundColor = .red
+        config.baseForegroundColor = .white
+        config.buttonSize = .large
+        config.cornerStyle = .medium
+        let btn = UIButton(configuration: config)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(didTapSubscribe), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var nameLb: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Taylor Swift"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    private lazy var emailLb: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "tswift13@gmail.com"
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
+    
+    private lazy var personStackView: UIStackView = {
+           let vw = UIStackView()
+            vw.axis = .vertical
+            vw.spacing = 8
+            vw.translatesAutoresizingMaskIntoConstraints = false
+            return vw
+        }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension PersonView {
+    
+    func setup(){
+        // Background config
+        self.layer.cornerRadius = 10
+        self.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(personStackView)
+        // to work as a VStack
+        personStackView.addArrangedSubview(nameLb)
+        personStackView.addArrangedSubview(emailLb)
+        personStackView.addArrangedSubview(subscribeBtn)
+        setConstraints()
+    }
+    
+    func setConstraints(){
+        NSLayoutConstraint.activate([
+            // STACK
+            personStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            personStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            personStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            personStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            
+        ])
+    }
+    
+    @objc func didTapSubscribe(sender: UIButton){
+        print("subscribing")
+    }
+}
